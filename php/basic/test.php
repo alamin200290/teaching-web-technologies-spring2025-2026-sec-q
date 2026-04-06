ab34c24619b3cb32a119d6a0569c60d9d2fcd177
alamin200290 <amin.se.aiub@gmail.com>
2026-04-06 14:34:54 +0600

    intro PHP

---
+++ php/basic/test.php
@@ -0,0 +1,38 @@
+<?php
+
+    $name = "alamin";
+    // $id = 12;
+    // $cgpa = 3.5;
+
+    //$std = ['alamin', 12, 3.5];
+    //$std = array('alamin', 12, 3.5);
+    // $stds = [
+    //             ['alamin', 12, 3.5],
+    //             ['alamin', 12, 3.5],
+    //             ['alamin', 12, 3.5]
+    //         ];
+    // $stds[1][1];
+
+    $std = ['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5];
+    //$std['name'];
+
+    $stds = [
+        's1'=>['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5],
+        's2'=>['name'=>'abc', 'id'=>2, 'cgpa'=>3.2],
+        's3'=>['name'=>'xyz', 'id'=>3, 'cgpa'=>3.1]
+    ];
+
+    //$stds['s3'];
+
+    echo "test <br>";
+    // echo $std['name'];
+    //print($name);
+    //print_r($stds['s3']);
+    //var_dump($std);
+    echo $std["name"]."-".$std["id"]."-".$std["cgpa"];
+
+    for($i=0; $i<10; $i++){
+        echo "<h1>".$std['name']."</h1>";
+    }
+
+?>
